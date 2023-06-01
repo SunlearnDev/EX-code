708f5c043dd64dfed3f45e85bc90df5d2a9883a7
Tai-TC <taitc.1440101226679@vtc.edu.vn>
2023-06-01 18:20:17 +0700

    js

--- baitap_javascript/bt1.html/bai4js.js
+++ baitap_javascript/bt1.html/bai4js.js
@@ -1,3 +1,24 @@
 function myFunction (){
-    var  
+    var  iDname = document.getElementById('nhap').value;
+    switch(iDname){
+        case '1':
+            alert('nước suối 15k');
+            
+        case '2':
+            alert('Coca 17k');
+            break;
+        case '3':
+                alert('7 Up 17k');
+                break;
+        case '4':
+                alert('Pesi 17k');
+                break;
+        case '5':
+                alert('Nước mía 10k');
+                break;
+    default :
+        alert('nhạp không hợp lệ');
+        break;
+    };
+
 }
