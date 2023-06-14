411b241449edeff6bdacee45ec2222173d2b6fb9
Tai-TC <taitc.1440101226679@vtc.edu.vn>
2023-06-14 11:03:06 +0700

    login

--- baitap_javascript/login/login.js
+++ baitap_javascript/login/login.js
@@ -1,30 +1,43 @@
 var userName = [`abc`, `bac`, `cba`];
 var passname =[`123`,`456`,`789`];
 function dangnhap(){
-var uName =  document.getElementById('txt_use').value;
-var uPass = document.getElementById('txt_pass').value;
+    // ------------------------------------------------------
+    // -------------------------Cách 1-----------------------
+    // ------------------------------------------------------
+// var uName =  document.getElementById('txt_use').value;
+// var uPass = document.getElementById('txt_pass').value;
 // for (var i = 0; i < userName.length; i++){
-//     if(uName == userName[i]){
-//      if(uPass == passname[i]){
+//     if(uName === userName[i]){
+//         console.log(userName[i]);
+//      if(uPass === passname[i]){
+//         console.log(passname[i]);
 //         window.location.href = `https://xem1080.com/cho-san-cong-ly/tap-1`;
 //     }
 //     else{
 //         alert(`that bai`);
 //     }
-    
+//     break;
 // } else {
 //   alert('Thất bại');
 // }
 // }
+// -----------------------------end---------------------------------
+
+    // ------------------------------------------------------
+    // -------------------------Cách 2-----------------------
+    // ------------------------------------------------------
+
+    // ------dùng phương thức includes kiểm tra dữ liệu Boolean
 if(userName.includes(uName) ){
+    
     var indexx =userName.indexOf(uName);
+    // nếu tồn tại tiếp tục dùng indexOf để tương ứng với với mảng cùng vị trí
     if(passname[indexx]===uPass){
         window.location.href = `https://xem1080.com/cho-san-cong-ly/tap-1`;
     }
     else{
         alert(`that bai`);
     }
-    
 } else {
   alert('Thất bại');
 }
---
+++ form-xuatdulieu/data.js
@@ -0,0 +1,30 @@
+var man = [
+    {														
+        id: 1 ,													
+        name: "Quần Nam",													
+        code: "TC1025011BA",													
+        price :"200000",													
+        image:"https://channel.mediacdn.vn/428462621602512896/2022/5/11/vulcano-1-1652263546844522213228.jpg"                               
+    },													
+    {														
+        id: 2 ,													
+        name: "Quần Nam",													
+        code: "TC1025011BA",													
+        price :"200000",													
+        image:"https://bizweb.dktcdn.net/100/426/984/files/phong-cach-classic-nam.png?v=1640061366799"													
+    },												
+    {														
+        id: 3 ,													
+        name: "Quần Nam",													
+        code: "TC1025011BA",													
+        price :"200000",													
+        image:"https://bizweb.dktcdn.net/100/426/984/files/phong-cach-classic-nam.png?v=1640061366799"                                                   
+    },												
+    {														
+        id: 4 ,													
+        name: "Quần Nam",													
+        code: "TC1025011BA",													
+        price :"200000",													
+        image:"https://channel.mediacdn.vn/428462621602512896/2022/5/11/vulcano-1-1652263546844522213228.jpg"                               
+    }
+]
